aa0431ea56e0c4a67127d96d87000046d19dff6e
walchko <walchko@users.noreply.github.com>
2017-12-26 15:08:32 -0700

    Initial commit

---
+++ zeromq/build-zmq.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 2 ]]; then
+  echo "Please supply an OpenCV version number"
+  echo "ex: ./build-zmq.sh 1.2.3"
+fi
+
+echo "$USER"
+echo "start ---------------"
+
+ZMQ_VERSION=$1
+CURRDIR=$(pwd)
+ZMQ_INSTALL_DIR="$CURRDIR/zeromq-dpkg/usr/local"
+
+echo ""
+echo "-------------------------------------------------"
+echo "Installing to: $ZMQ_INSTALL_DIR"
+echo "-------------------------------------------------"
+echo ""
+
+
+if [[ "$OSTYPE" == "linux-gnu" ]] || [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
+	echo "Upgrading software"
+	echo ""
+	# apt-get update
+	apt-get -y upgrade
+	apt-get -y install build-essential cmake git pkg-config
+	apt-get -y install libtool autoconf automake uuid-dev libunwind-dev
+
+	echo "System ready"
+	echo ""
+fi
+
+if [ ! -f zeromq-$ZMQ_VERSION.tar.gz ]; then
+	wget https://github.com/zeromq/libzmq/releases/download/v$ZMQ_VERSION/zeromq-$ZMQ_VERSION.tar.gz
+else
+	echo "Using previously downloaded file"
+	rm -fr zeromq-$ZMQ_VERSION
+fi
+
+# setup things
+tar zxf zeromq-$ZMQ_VERSION.tar.gz
+cd zeromq-$ZMQ_VERSION
+./configure --prefix=$ZMQ_INSTALL_DIR
+
+# make and install
+make -j4
+make install
